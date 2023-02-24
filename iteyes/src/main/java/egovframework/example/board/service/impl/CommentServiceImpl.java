a7581f267246d7826ffc5a0d74c852070073b0a1
PNut <105850794+peanut0114@users.noreply.github.com>
2023-02-24 16:01:26 +0900

    23-02-23 프젝 폴더 분리해서 생성 - 댓글, xml 설정파일

---
+++ iteyes/src/main/java/egovframework/example/board/service/impl/CommentServiceImpl.java
@@ -0,0 +1,41 @@
+package egovframework.example.board.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import org.springframework.stereotype.Service;
+
+import egovframework.example.board.service.CommentService;
+import egovframework.example.board.service.CommentVO;
+
+@Service("commentService")
+public class CommentServiceImpl extends EgovAbstractServiceImpl implements CommentService {
+
+	@Resource(name = "commentMapper")
+	CommentMapper mapper;
+
+	@Override
+	public int insertComment(CommentVO vo) throws Exception {
+		return mapper.insertComment(vo);
+	}
+
+	@Override
+	public int updateComment(CommentVO vo) throws Exception {
+		return mapper.updateComment(vo);
+	}
+
+	@Override
+	public int deleteComment(CommentVO vo) throws Exception {
+		int id = vo.getId();
+		return mapper.deleteComment(id);
+	}
+
+	@Override
+	public List<CommentVO> getComment(CommentVO vo) throws Exception {
+		return mapper.getComment(vo);
+	}
+	
+	
+}
